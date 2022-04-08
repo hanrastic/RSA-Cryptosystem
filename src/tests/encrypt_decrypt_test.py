8780dfb1fb7d4ab87049badb2954afdf54cc9bc4
Joonas Maisala <joonas-maisala@hotmail.com>
2022-04-08 18:36:52 +0300

    Testing for Encrypting adn decrypting ready 100%

---
+++ src/tests/encrypt_decrypt_test.py
@@ -0,0 +1,21 @@
+import unittest
+from app.key_generator import KeyGenerator
+from app.encrypt_decrypt import Encrypt, Decrypt
+
+
+class TestEncrypt(unittest.TestCase):
+    def setUp(self):
+        self.keygenerator = KeyGenerator(1024)
+        self.public, self.private = self.keygenerator.generate_keys()
+        self.test_message = "This is an example message to be encrypted."
+        self.test_message2 = "This is an example message to be encrypteb."
+        self.test_message_length = len(self.test_message.encode())
+        self.encrypted_message = Encrypt().encrypt_message(self.test_message, self.public)
+
+    def test_encrypting(self):
+        self.assertEqual(type(self.encrypted_message), int)
+
+    def test_decrypting(self):
+        self.decrypted_message = Decrypt().decrypt_message(self.encrypted_message, self.test_message_length, self.private)
+        self.assertEqual(self.decrypted_message, self.test_message)
+        self.assertNotEqual(self.decrypted_message, self.test_message2)
---
+++ src/tests/key_generator_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from app.key_generator import KeyGenerator
+
+
+class TestKeyGenerator(unittest.TestCase):
+    def setUp(self):
+        self.keygenerator = KeyGenerator(1024)
+        self.public, self.private = self.keygenerator.generate_keys()
+
+    def test_key_generation(self):
+        self.assertEqual(True, self.keygenerator.public_key is not None)
+        self.assertEqual(True, self.keygenerator.private_key is not None)
+
+    def test_prime_number_generation(self):
+        p,q = self.keygenerator.generate_primes()
+        self.assertEqual(True, self.keygenerator.is_prime(p))
+        self.assertEqual(True, self.keygenerator.is_prime(q))
+
+    def test_is_prime_function(self):
+        self.assertEqual(True, self.keygenerator.is_prime(3700889))
+        self.assertEqual(True, self.keygenerator.is_prime(982805851549327779922977967973))
+        self.assertEqual(True, self.keygenerator.is_prime(7))
+        self.assertEqual(True, self.keygenerator.is_prime(328054627542497))
+        self.assertEqual(True, self.keygenerator.is_prime(2))
+
+    def test_is_not_prime_function(self):
+        self.assertEqual(False, self.keygenerator.is_prime(9))
+        self.assertEqual(False, self.keygenerator.is_prime(213))
+        self.assertEqual(False, self.keygenerator.is_prime(1245443))
+        self.assertEqual(False, self.keygenerator.is_prime(38299301928383102938213098))
+        
+    def test_keys(self):
+        self.assertEqual(self.public, self.keygenerator.public_key)
+        self.assertEqual(self.private, self.keygenerator.private_key)
